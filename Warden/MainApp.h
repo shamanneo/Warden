e23f275b4a1f593a04aab74d215a6dc70c9230b6
shaman <shaman.neo@outlook.com>
2022-05-09 00:33:31 +0900

    [Warden] INIT

---
+++ Warden/MainApp.h
@@ -0,0 +1,18 @@
+#include "MainWnd.h"
+
+class CMainApp
+{
+    private :
+        CMainWnd m_MainWnd ; 
+    public :
+        CMainApp() ; 
+        ~CMainApp() ; 
+    public :
+        int Run(HINSTANCE hInstance, int nCmdShow) ; 
+    public :    
+        static CMainApp &GetInstance() ; 
+        static void Release() ; 
+} ;
+
+
+
